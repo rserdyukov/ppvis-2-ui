910521aece7e6575764daab11e381ff27bc917ae
Your NameWetGrapes-mat <matvey.diubuilo@icloud.com>
2022-04-08 01:13:44 +0300

    MVC

--- example1_MVC/main.py
+++ example1_MVC/main.py
@@ -10,58 +10,58 @@ Config.set("graphics", "width", "300")
 Config.set("graphics", "height", "300")
 
 
-class TicTackToeView(App):
-    def __init__(self, controller, **kw):
+class TicTackToeView:
+    def __init__(self, model, **kw):
         super().__init__(**kw)
+        self.model = model
+        self.buttons = self.model.buttons
+
+    def build(self):
+        root = BoxLayout(orientation="vertical", padding=5)
+        grid = GridLayout(cols=3)
+        for button in self.buttons['map']:
+            grid.add_widget(button)
+        root.add_widget(grid)
+        root.add_widget(self.buttons['restart'])
+        return root
+
+    def tic_tac_toe(self, flag_win, item_color):
+        if flag_win:
+            color = [0, 1, 0, 1]
+            for i in item_color:
+                self.buttons['map'][i].color = color
+
+    def restart(self, arg):
+        for button in self.buttons['map']:
+            button.color = [0, 0, 0, 1]
+            button.text = ""
+
+    def cell_changes(self, arg, text):
+        arg.text = text
+
+
+class TicTackToeModel:
+    def __init__(self, controller):
+        self.buttons = {}
+        self.switch = True
         self.controller = controller
-        self.buttons = []
-        self.gr = self.build_buttons()
 
     def build_buttons(self):
-        grid = GridLayout(cols=3)
+        buttons = []
         for _ in range(9):
             button = Button(
                 color=[0, 0, 0, 1],
                 font_size=24,
                 disabled=False,
-                on_press=self.tic_tac_toe
+                on_press=self.controller.tic_tac_toe
             )
-            self.buttons.append(button)
-            grid.add_widget(button)
-        return grid
-
-    def build(self):
-        self.title = "Tic Tack Toe"
-        root = BoxLayout(orientation="vertical", padding=5)
-        root.add_widget(self.gr)
-        root.add_widget(
-            Button(
-                text="Restart",
-                size_hint=[1, .1],
-                on_press=self.restart
-            )
+            buttons.append(button)
+        self.buttons['map'] = buttons
+        self.buttons['restart'] = Button(
+            text="Restart",
+            size_hint=[1, .1],
+            on_press=self.controller.restart
         )
-        return root
-
-    def tic_tac_toe(self, arg):
-        arg.text = self.controller.tic_tac_toe(arg)
-        flag_win, item_color = self.controller.check_win()
-        if flag_win:
-            color = [0, 1, 0, 1]
-            for i in item_color:
-                self.buttons[i].color = color
-
-    def restart(self, arg):
-        self.controller.restart(arg)
-        for button in self.buttons:
-            button.color = [0, 0, 0, 1]
-            button.text = ""
-
-
-class TicTackToeModel:
-    def __init__(self):
-        self.buttons = []
-        self.switch = True
 
     def tic_tac_toe(self, arg):
         arg.disabled = True
@@ -76,12 +76,12 @@ class TicTackToeModel:
             (0, 4, 8), (2, 4, 6),  # D
         )
 
-        vector = lambda item: [self.buttons[x].text for x in item]
+        vector = lambda item: [self.buttons['map'][x].text for x in item]
 
         for item in coordinate:
             if vector(item).count('X') == 3 or vector(item).count('O') == 3:
                 win = True
-                for button in self.buttons:
+                for button in self.buttons['map']:
                     button.disabled = True
                 return win, item
 
@@ -89,39 +89,40 @@ class TicTackToeModel:
 
     def restart(self, arg):
         self.switch = True
-        for button in self.buttons:
+        for button in self.buttons['map']:
             button.disabled = False
 
 
 class TicTackToeController:
-    def __init__(self, model):
-        self.model = model
-        self.view = TicTackToeView(self)
-        self.share_buttons()
+    def __init__(self):
+        self.model = TicTackToeModel(self)
+        self.view = TicTackToeView(self.model)
 
     def tic_tac_toe(self, arg):
-        return self.model.tic_tac_toe(arg)
+        text = self.model.tic_tac_toe(arg)
+        self.view.cell_changes(arg, text)
+        flag_win, item_color = self.model.check_win()
+        self.view.tic_tac_toe(flag_win, item_color)
 
-    def share_buttons(self):
-        self.model.buttons = self.view.buttons
+    def get_buttons(self):
+        return self.model.buttons
 
     def get_screen(self):
+        self.model.build_buttons()
         return self.view.build()
-
-    def check_win(self):
-        return self.model.check_win()
 
     def restart(self, arg):
         self.model.restart(arg)
+        self.view.restart(arg)
 
 
 class PassMVC(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.model = TicTackToeModel()
-        self.controller = TicTackToeController(model=self.model)
+        self.controller = TicTackToeController()
 
     def build(self):
+        self.title = "Tic Tack Toe"
         return self.controller.get_screen()
 
 
