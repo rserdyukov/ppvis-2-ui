f3630c5de56ff782878658842beb4d343649580f
Семён <81536102+Semen-proger@users.noreply.github.com>
2022-04-10 18:28:40 +0300

    Refactor to MVP

--- example3/tic_tac_toe.py
+++ example3/tic_tac_toe.py
@@ -11,38 +11,74 @@ Config.set("graphics", "width", "300")
 Config.set("graphics", "height", "300")
 
 
-class TicTackToe(App):
+class Model:
     def __init__(self):
-        super().__init__()
-        self.buttons = []
-        self.switch = True
-
-    def tic_tac_toe(self, arg):
-        arg.disabled = True
-        arg.text = 'X' if self.switch else 'O'
-        self.switch = not self.switch
-
-        coordinate = (
+        self.coordinate = (
             (0, 1, 2), (3, 4, 5), (6, 7, 8),  # X
             (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Y
             (0, 4, 8), (2, 4, 6),  # D
         )
+        self.items = [''] * 9
+        self.switch = True
 
-        vector = lambda item: [self.buttons[x].text for x in item]
+    def click(self, index):
+        self.items[index] = 'X' if self.switch else 'O'
+        self.switch = not self.switch
+
+    def getItems(self):
+        return self.items
+
+    def restart(self):
+        self.items = [''] * 9
+        self.switch = True
+
+    def tic_tac_toe(self):
+        vector = lambda item: [self.items[x] for x in item]
+
+        for item in self.coordinate:
+            if vector(item).count('X') == 3 or vector(item).count('O') == 3:
+                return item
+
+
+class Presenter:
+    def __init__(self):
+        self.model = Model()
+
+    def click(self, index):
+        self.model.click(index)
+
+    def getItems(self):
+        return self.model.items
+
+    def restart(self):
+        self.model.restart()
+
+    def tic_tac_toe(self):
+        return self.model.tic_tac_toe()
+
+
+class TicTackToe(App):
+    def __init__(self):
+        super().__init__()
+        self.buttons = []
+        self.controller = Presenter()
+
+    def click(self, arg):
+        arg.disabled = True
+        self.controller.click(self.buttons.index(arg))
+        arg.text = self.controller.getItems()[self.buttons.index(arg)]
 
         color = [0, 1, 0, 1]
 
-        for item in coordinate:
-            if vector(item).count('X') == 3 or vector(item).count('O') == 3:
-                win = True
-                for i in item:
-                    self.buttons[i].color = color
-                for button in self.buttons:
-                    button.disabled = True
-                break
+        item = self.controller.tic_tac_toe()
+        if item is not None:
+            for i in item:
+                self.buttons[i].color = color
+            for button in self.buttons:
+                button.disabled = True
 
     def restart(self, arg):
-        self.switch = True
+        self.controller.restart()
 
         for button in self.buttons:
             button.color = [0, 0, 0, 1]
@@ -60,7 +96,7 @@ class TicTackToe(App):
                 color=[0, 0, 0, 1],
                 font_size=24,
                 disabled=False,
-                on_press=self.tic_tac_toe
+                on_press=self.click
             )
             self.buttons.append(button)
             grid.add_widget(button)
